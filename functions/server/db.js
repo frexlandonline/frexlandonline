734075b47bee85d40bc240c2aeaa2deeb8ab09a4
Pablo Oscar Forlani <pablo.forlani.86@gmail.com>
2026-08-07 06:34:26 -0300

    Fix Firebase initialization on local server

--- functions/server/db.js
+++ functions/server/db.js
@@ -2,7 +2,17 @@ import admin from 'firebase-admin';
 
 // Initialize Firebase Admin (Functions environment automatically provides credentials)
 if (!admin.apps.length) {
-  admin.initializeApp();
+  if (process.env.FIREBASE_PRIVATE_KEY) {
+    admin.initializeApp({
+      credential: admin.credential.cert({
+        projectId: process.env.FIREBASE_PROJECT_ID,
+        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+        privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n')
+      })
+    });
+  } else {
+    admin.initializeApp();
+  }
 }
 
 const firestoreDb = admin.firestore();
